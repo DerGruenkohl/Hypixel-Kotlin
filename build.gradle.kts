eff8c344fc2d566a812541981d3e44ffc5bcfbb3
DerGruenkohl <paco@dergruenkohl.com>
2025-02-05 17:39:08 +0100

    update buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 private val ktor_version: String by project
 private val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.gradleup.shadow") version "9.0.0-beta7"
     `maven-publish`
 }
 
@@ -24,6 +27,21 @@ dependencies {
 
     testImplementation(kotlin("test"))
 }
+tasks.withType<ShadowJar> {
+    mergeServiceFiles()
+    archiveFileName = "Hypixel-Kotlin.jar"
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifact(tasks["shadowJar"])
+        }
+    }
+}
+
+
 
 tasks.test {
     useJUnitPlatform()
